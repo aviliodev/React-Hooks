481a6769a9b682b5485f045db4d44b49406dbdcd
aviliodev <quidralion@gmail.com>
2021-06-30 17:44:25 -0600

    1. primer Commit

---
+++ src/components/Characters.jsx
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react'
+import '../styles/characters.css'
+
+const Characters = () => {
+    /**
+     * Lógica de useState
+     * constante donde internamente estructuramos los elementos que necesitamos
+     * de useState y lo iniciamos como un vector vacío
+     */
+    const [characters, setCharacters] = useState([]);
+    /**
+     * Lógica de useEffect
+     * es una función con 2 parámetros
+     * el primero es una función anónima donde va a estar la lógica
+     * el segundo es una variable que esta escuchando si hay cambios.
+     */
+    useEffect(() => {
+        // useEffect llama a fetch, el cual obtiene la informacion de la api de RickAndMorty (lo cambié a la api de Gibli)
+        // fetch('https://rickandmortyapi.com/api/character/') //API DE RICK Y MORTY
+        fetch('http://hp-api.herokuapp.com/api/characters/')
+        .then(response => response.json())
+        // .then(data => setCharacters(data.results)) //Para que funcione Api de Rick y Morty
+        .then(data => setCharacters(data))
+    },[]); // La variable que escuchará si hay cambios, en este caso no se usa y por eso se coloca un arreglo vacio.
+
+    /** 
+     * Nombre de la pelicula
+     * Iteramos por cada uno de los elementos
+     */
+    return (
+        <div className="Characters">
+            {characters.map(hcharacter => (
+                <div className="ficha">
+                    <div className="datos">
+                        <h2>{hcharacter.name}</h2>
+                        <h3>Casa : {hcharacter.house}</h3>    
+                    </div>
+                    <img src={hcharacter.image} alt={hcharacter.name}></img>  
+               
+                </div>
+            
+            ))}
+
+        </div>
+    );
+};
+
+export default Characters;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+const Header = () => {
+    const [darkMode, setDarkMode] = useState(false);
+    const handleClick = () => {
+        setDarkMode(!darkMode);
+    }
+    return (
+        <div className="Header">
+            <h1>Harry Potter API</h1>
+            <button type="button" onClick={handleClick}> {darkMode ? 'Dark Mode' : 'Light Mode'}</button>
+        </div>
+    )
+};
+
+export default Header;
